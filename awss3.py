de5a8f676bf147c45e1b2d236bff687e90014067
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-05-19 02:27:14 -0400

    Adding more stuff for .env and adding awss3 class

---
+++ awss3.py
@@ -0,0 +1,18 @@
+from lib import *
+from Remote import *
+import boto3
+from botocore.exceptions import ClientError
+class awss3(Remote):
+    def __init__(self):
+        self.s3 = boto3.client('s3')
+    #def get_list_of_modified_files
+    def upload_file(self,file_name, bucket):
+        object_name = file_name
+        try:
+            response = self.s3.upload_file(file_name, bucket, object_name)
+        except ClientError as e:
+            print(e)
+            return False
+        print("FILE" + str(file_name) + "UPLOADED SUCESSFULLY TO BUCKET" + str(bucket))
+        return True
+    
