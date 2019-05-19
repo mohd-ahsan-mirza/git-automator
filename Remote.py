d72cffe685dd9c67a8883e92379a9fa991c97ece
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-05-18 20:07:00 -0400

    Bug fix

--- Remote.py
+++ Remote.py
@@ -1,5 +1,5 @@
 from lib import *
 class Remote:
     def retrieve_password(self,value):
-        return subprocess.check_output(os.getenv("PASSWORD_RETRIEVAL_COMMAND") + " -get " + str(value) + " --return-value",shell=True).decode("utf-8")
+        return subprocess.check_output(os.getenv("PASSWORD_RETRIEVAL_COMMAND") + " -get " + str(value) + " --return-value",shell=True).decode("utf-8").strip("\n")
 
