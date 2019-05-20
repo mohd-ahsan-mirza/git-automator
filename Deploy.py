2b6864c4cee879431ef7ad25c8363f4ab9e5f843
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-05-20 02:31:09 -0400

    Adding delete functionality

--- Deploy.py
+++ Deploy.py
@@ -15,13 +15,15 @@ class Deploy(Git,awss3):
         self.git.deploy_to_remote_server()
     def deploy_to_awss3(self,env):
         files_to_upload = self.git.get_added_files() + self.git.get_modified_files()
+        files_to_delete = self.git.get_deleted_files()
+        files_to_upload = [x for x in files_to_upload if x not in files_to_delete]
         print("-------Files to Upload----")
         print(files_to_upload)
-        #files_to_delete = self.get_deleted_files()
-        #print("----Files to Delete------")
-        #print(files_to_delete)
+        print("----Files to Delete------")
+        print(files_to_delete)
         bucket = os.getenv("TEST_BUCKET")
         #if env == "Prod"
         for filename in files_to_upload:
             self.awss3.upload_file(filename,bucket)
-
+        for filename in files_to_delete:
+            self.awss3.delete_file(filename,bucket)
--- awss3.py
+++ awss3.py
@@ -15,4 +15,11 @@ class awss3(Remote):
             return False
         print("FILE" + str(file_name) + " UPLOADED SUCESSFULLY TO BUCKET " + str(bucket))
         return True
+    def delete_file(self,file_name,bucket):
+        try:
+            self.s3.delete_object(Bucket=bucket, Key=file_name)
+        except ClientError as e:
+            print(e)
+            return False
+        print("FILE" + str(file_name) + " DELETED SUCESSFULLY TO BUCKET " + str(bucket))
     
