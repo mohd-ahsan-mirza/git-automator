4502bdb4f18fdf6d1f73788aba4cf5a1cfd42251
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-05-19 20:37:34 -0400

    Change

--- Deploy.py
+++ Deploy.py
@@ -13,11 +13,15 @@ class Deploy(Git,awss3):
         self.git.push_local()
     def deploy_to_remote_server(self):
         self.git.deploy_to_remote_server()
-    def deploy_to_awss3(self):
+    def deploy_to_awss3(self,env):
         files_to_upload = self.git.get_added_files() + self.git.get_modified_files()
         print("-------Files to Upload----")
         print(files_to_upload)
-        files_to_delete = self.get_deleted_files()
-        print("----Files to Delete------")
-        print(files_to_delete)
-        
+        #files_to_delete = self.get_deleted_files()
+        #print("----Files to Delete------")
+        #print(files_to_delete)
+        bucket = os.getenv("TEST_BUCKET")
+        #if env == "Prod"
+        for filename in files_to_upload:
+            self.awss3.upload_file("../"+filename,bucket)
+
--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 from lib import *
 from Deploy import *
 deploy = Deploy()
-deploy.push_to_git()
 if os.getenv("REMOTE_ENV") == 'remote-server':
+    deploy.push_to_git()
     deploy.deploy_to_remote_server()
     exit()
 if os.getenv("REMOTE_ENV") == "AWS":
     print("-----Added files---------")
-    print(deploy.deploy_to_awss3())
+    deploy.deploy_to_awss3(os.getenv("AWS_ENV"))
+    deploy.push_to_git()
     exit()
 print("No Remote environment selected")
 exit()
