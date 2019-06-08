8583f80e7141a8dd46a7902238d8258ee7b1d8e0
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-06-08 01:30:49 -0400

    Adding upload file functionality again

--- awss3.py
+++ awss3.py
@@ -8,15 +8,25 @@ class awss3(Remote):
             self.s3 = boto3.client('s3')    
     def upload_file(self,file_name, bucket):
         object_name = file_name
+        content_type = splitext(file_name)[1].strip(".")
+        upload_html_content = True
+        response = ""
+        if content_type not in ['html','css','js']:
+            content_type = 'plain'
+            upload_html_content = False
         try:
-            response = self.s3.put_object(
-                ACL ='public-read',
-                Body = open(os.getenv("RELATIVE_PATH_TO_PROJECT_DIRECTORY") + file_name,"r").read(),
-                Bucket = bucket,
-                Key = object_name,
-                CacheControl="max-age=0,no-cache,no-store,must-revalidate",
-                ContentType="text/"+splitext(file_name)[1].strip("."),
-            )
+            if upload_html_content:
+                response = self.s3.put_object(
+                    ACL ='public-read',
+                    Body = open(os.getenv("RELATIVE_PATH_TO_PROJECT_DIRECTORY") + file_name,"r").read(),
+                    Bucket = bucket,
+                    Key = object_name,
+                    CacheControl="max-age=0,no-cache,no-store,must-revalidate",
+                    ContentType="text/"+content_type,
+                )
+            else:
+                print("Please manually change the content type of the file on the S3 bucket if required")
+                response = self.s3.upload_file(os.getenv("RELATIVE_PATH_TO_PROJECT_DIRECTORY") + file_name,bucket,file_name)
         except ClientError as e:
             print(e)
             return False
